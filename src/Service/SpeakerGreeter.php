6c8be0b347ce1e787bf153393c03addb7f61cad8
Oliver Davies <oliver@oliverdavies.uk>
2016-03-03 01:03:42 +0000

    Add shout parameter

--- src/Service/SpeakerGreeter.php
+++ src/Service/SpeakerGreeter.php
@@ -5,10 +5,27 @@ namespace Drupal\dclondon\Service;
 class SpeakerGreeter implements GreeterInterface {
 
   /**
+   * @var boolean
+   */
+  private $shout;
+
+  public function __construct($shout) {
+    $this->shout = $shout;
+  }
+
+
+  /**
    * {@inheritdoc}
    */
   public function greet($name) {
-    return ucfirst($name);
+    if ($this->shout) {
+      $name = strtoupper($name);
+    }
+    else {
+      $name = ucfirst($name);
+    }
+
+    return $name;
   }
 
 }
